a79770d71b3e797ea736b532d06f319491353f5f
dowczarek <d_owczarek@outlook.com>
2021-11-20 17:46:44 +0100

    Create CRUD for Task - first Entity and Repository

---
+++ src/main/java/com/dowczarek/model/Task.java
@@ -0,0 +1,38 @@
+package com.dowczarek.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+class Task {
+    @Id
+    private int id;
+    private String description;
+    private boolean done;
+
+    int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    boolean isDone() {
+        return done;
+    }
+
+    void setDone(boolean done) {
+        this.done = done;
+    }
+}
