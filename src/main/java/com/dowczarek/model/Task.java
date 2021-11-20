b831c2c1e51dbfb68577062cd3cb0620e47ab3ff
dowczarek <d_owczarek@outlook.com>
2021-11-20 19:03:55 +0100

    Add validation configuration and annotation

--- src/main/java/com/dowczarek/model/Task.java
+++ src/main/java/com/dowczarek/model/Task.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
@@ -12,6 +13,7 @@ class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
     private boolean done;
 
