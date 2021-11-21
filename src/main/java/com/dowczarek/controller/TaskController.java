9a441b5cac7401104b7e34008afd3a1a5c5973cc
dowczarek <d_owczarek@outlook.com>
2021-11-21 19:54:50 +0100

    TaskController - add a method for create the task

--- src/main/java/com/dowczarek/controller/TaskController.java
+++ src/main/java/com/dowczarek/controller/TaskController.java
@@ -8,11 +8,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,6 +24,12 @@ class TaskController {
 
     TaskController(TaskRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate) {
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/tasks/" + result.getId())).build();
     }
 
     @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
