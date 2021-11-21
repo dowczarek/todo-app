e2cdd1d624e7c0a30547d68183b73d25fe3126f4
dowczarek <d_owczarek@outlook.com>
2021-11-21 17:13:58 +0100

    TaskController - add pageable method to handle pagination and sorting request

--- src/main/java/com/dowczarek/controller/TaskController.java
+++ src/main/java/com/dowczarek/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.dowczarek.controller;
 import com.dowczarek.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,15 @@ class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping("/tasks")
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
     ResponseEntity<?> readAllTasks() {
         logger.warn("Exposing all the tasks!");
-
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<?> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page));
     }
 }
