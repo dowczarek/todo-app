876d9e52328381e6b5c4688e3f4f47b797f81d98
dowczarek <d_owczarek@outlook.com>
2021-11-22 22:32:20 +0100

    TaskConfigurationProperties - create custom ConfigurationProperties

---
+++ src/main/java/com/dowczarek/TaskConfigurationProperties.java
@@ -0,0 +1,18 @@
+package com.dowczarek;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("task")
+public class TaskConfigurationProperties {
+    private boolean allowMultipleTaskFromTemplate;
+
+    public boolean isAllowMultipleTaskFromTemplate() {
+        return allowMultipleTaskFromTemplate;
+    }
+
+    public void setAllowMultipleTaskFromTemplate(boolean allowMultipleTaskFromTemplate) {
+        this.allowMultipleTaskFromTemplate = allowMultipleTaskFromTemplate;
+    }
+}
--- src/main/java/com/dowczarek/controller/InfoController.java
+++ src/main/java/com/dowczarek/controller/InfoController.java
@@ -1,23 +1,27 @@
 package com.dowczarek.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.dowczarek.TaskConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class InfoController {
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${my.prop}")
-    private String myProp;
+    private final DataSourceProperties dataSource;
+    private final TaskConfigurationProperties myProp;
+
+    InfoController(DataSourceProperties dataSource, TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
 
     @GetMapping("/info/url")
     String url() {
-        return url;
+        return dataSource.getUrl();
     }
 
     @GetMapping("/info/prop")
-    String myProp() {
-        return myProp;
+    boolean myProp() {
+        return myProp.isAllowMultipleTaskFromTemplate();
     }
 }
