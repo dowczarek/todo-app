fa71bf236b7112afeb1aea41edf1309acfef3d8a
dowczarek <d_owczarek@outlook.com>
2021-12-02 21:12:55 +0100

    Test @Embedded and @Embeddable annotation

--- src/main/java/com/dowczarek/model/Task.java
+++ src/main/java/com/dowczarek/model/Task.java
@@ -1,5 +1,6 @@
 package com.dowczarek.model;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
-public class Task extends BaseAuditableEntity {
+public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -18,6 +19,8 @@ public class Task extends BaseAuditableEntity {
     private String description;
     private boolean done;
     private LocalDateTime deadline;
+    @Embedded
+    private Audit audit = new Audit();
 
     Task() {
     }
