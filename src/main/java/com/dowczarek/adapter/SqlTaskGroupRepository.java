6483904939690d06cc05d2a243b13fb292f25c51
dowczarek <d_owczarek@outlook.com>
2022-01-14 22:37:12 +0100

    Fix N+1 problem with findAll() in SqlTaskGroupRepository

--- src/main/java/com/dowczarek/adapter/SqlTaskGroupRepository.java
+++ src/main/java/com/dowczarek/adapter/SqlTaskGroupRepository.java
@@ -3,6 +3,7 @@ package com.dowczarek.adapter;
 import com.dowczarek.model.TaskGroup;
 import com.dowczarek.model.TaskGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
     @Override
+    @Query("select distinct g from TaskGroup g join fetch g.tasks")
     List<TaskGroup> findAll();
 }
