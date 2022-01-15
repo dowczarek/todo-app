72f8d492ff9c6fbe99ef7ef7d1496938a1c91485
dowczarek <d_owczarek@outlook.com>
2022-01-15 16:46:08 +0100

    Added TaskGroupService

---
+++ src/main/java/com/dowczarek/logic/TaskGroupService.java
@@ -0,0 +1,43 @@
+package com.dowczarek.logic;
+
+import com.dowczarek.model.TaskGroup;
+import com.dowczarek.model.TaskGroupRepository;
+import com.dowczarek.model.TaskRepository;
+import com.dowczarek.model.projection.GroupReadModel;
+import com.dowczarek.model.projection.GroupWriteModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskGroupService {
+    private final TaskGroupRepository repository;
+    private final TaskRepository taskRepository;
+
+    TaskGroupService(TaskGroupRepository repository, TaskRepository taskRepository) {
+        this.repository = repository;
+        this.taskRepository = taskRepository;
+    }
+
+    public GroupReadModel create(GroupWriteModel source) {
+        TaskGroup result = repository.save(source.toGroup());
+
+        return new GroupReadModel(result);
+    }
+
+    public List<GroupReadModel> readAll() {
+        return repository.findAll().stream()
+                .map(GroupReadModel::new)
+                .collect(Collectors.toList());
+    }
+
+    public void toggleGroup(int groupId) {
+        if (taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)) {
+            throw new IllegalStateException("Group has undone tasks. Done all the task first.");
+        }
+        TaskGroup result = repository.findById(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("TaskGroup with given id not found."));
+        result.setDone(!result.isDone());
+    }
+}
