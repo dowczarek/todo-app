6d5438496dd496fb3d70d2c3493bbc3e4a6f7d61
dowczarek <d_owczarek@outlook.com>
2022-01-15 16:21:02 +0100

    Added Task DTO

---
+++ src/main/java/com/dowczarek/model/projection/GroupTaskReadModel.java
@@ -0,0 +1,29 @@
+package com.dowczarek.model.projection;
+
+import com.dowczarek.model.Task;
+
+public class GroupTaskReadModel {
+    private String description;
+    private boolean done;
+
+    GroupTaskReadModel(Task source) {
+        description = source.getDescription();
+        done = source.isDone();
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    boolean isDone() {
+        return done;
+    }
+
+    void setDone(boolean done) {
+        this.done = done;
+    }
+}
