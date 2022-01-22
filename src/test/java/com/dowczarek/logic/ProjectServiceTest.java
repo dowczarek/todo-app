3e4294a8b4b470e9de1c04ed29873da922eb2dfb
dowczarek <d_owczarek@outlook.com>
2022-01-22 23:05:58 +0100

    ProjectService - created first unit test with junit, mockito and assertj

---
+++ src/test/java/com/dowczarek/logic/ProjectServiceTest.java
@@ -0,0 +1,42 @@
+package com.dowczarek.logic;
+
+import com.dowczarek.TaskConfigurationProperties;
+import com.dowczarek.model.TaskGroupRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ProjectServiceTest {
+
+    @Test
+    @DisplayName("should throw IllegalStateException when configured to allow just 1 group and the other undone group exists")
+    void createGroup_noMultipleGroupsConfig_And_undoneGroupExists_throwsIllegalStateException() {
+        // given
+        var mockGroupRepository = mock(TaskGroupRepository.class);
+        when(mockGroupRepository.existsByDoneIsFalseAndProject_Id(anyInt())).thenReturn(true);
+        // and
+        var mockTemplate = mock(TaskConfigurationProperties.Template.class);
+        when(mockTemplate.isAllowMultipleTask()).thenReturn(false);
+        // and
+        var mockConfig = mock(TaskConfigurationProperties.class);
+        when(mockConfig.getTemplate()).thenReturn(mockTemplate);
+        // system under test
+        var toTest = new ProjectService(null, mockGroupRepository, mockConfig);
+
+        // when
+        var exception = catchThrowable(() -> toTest.createGroup(0, LocalDateTime.now()));
+
+        // then
+        assertThat(exception)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("one undone group");
+    }
+}
