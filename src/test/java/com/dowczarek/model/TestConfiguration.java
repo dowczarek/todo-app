f9871c35d55a413c2cd1cad89ac63dc72b54314d
dowczarek <d_owczarek@outlook.com>
2022-02-05 14:24:58 +0100

    Test TaskRepository - create bean in test configuration class

---
+++ src/test/java/com/dowczarek/model/TestConfiguration.java
@@ -0,0 +1,59 @@
+package com.dowczarek.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Configuration
+class TestConfiguration {
+    @Bean
+    @Profile("integration")
+    TaskRepository testTaskRepository() {
+        return new TaskRepository() {
+            private final Map<Integer, Task> tasks = new HashMap<>();
+
+            @Override
+            public List<Task> findAll() {
+                return new ArrayList<>(tasks.values());
+            }
+
+            @Override
+            public Page<Task> findAll(Pageable page) {
+                return null;
+            }
+
+            @Override
+            public Optional<Task> findById(Integer id) {
+                return Optional.ofNullable(tasks.get(id));
+            }
+
+            @Override
+            public boolean existsById(Integer id) {
+                return tasks.containsKey(id);
+            }
+
+            @Override
+            public boolean existsByDoneIsFalseAndGroup_Id(Integer groupId) {
+                return false;
+            }
+
+            @Override
+            public List<Task> findByDone(boolean done) {
+                return null;
+            }
+
+            @Override
+            public Task save(Task entity) {
+                return tasks.put(tasks.size() + 1, entity);
+            }
+        };
+    }
+}
