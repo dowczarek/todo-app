e1c2946a3f6f939909f84f77b91ac5b4be5b69e1
dowczarek <d_owczarek@outlook.com>
2022-02-05 16:46:38 +0100

    TaskControllerIntegrationTest - extract InMemoryTaskRepository

---
+++ src/test/java/com/dowczarek/logic/InMemoryTaskRepository.java
@@ -0,0 +1,61 @@
+package com.dowczarek.logic;
+
+import com.dowczarek.model.Task;
+import com.dowczarek.model.TaskRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+class InMemoryTaskRepository implements TaskRepository {
+    private final Map<Integer, Task> tasks = new HashMap<>();
+
+    @Override
+    public List<Task> findAll() {
+        return new ArrayList<>(tasks.values());
+    }
+
+    @Override
+    public Page<Task> findAll(Pageable page) {
+        return null;
+    }
+
+    @Override
+    public Optional<Task> findById(Integer id) {
+        return Optional.ofNullable(tasks.get(id));
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return tasks.containsKey(id);
+    }
+
+    @Override
+    public boolean existsByDoneIsFalseAndGroup_Id(Integer groupId) {
+        return false;
+    }
+
+    @Override
+    public List<Task> findByDone(boolean done) {
+        return null;
+    }
+
+    @Override
+    public Task save(Task entity) {
+        int key = tasks.size() + 1;
+        try {
+            var filed = Task.class.getDeclaredField("id");
+            filed.setAccessible(true);
+            filed.set(entity, key);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        tasks.put(key, entity);
+
+        return tasks.get(key);
+    }
+}
--- src/test/java/com/dowczarek/logic/TestLogicConfiguration.java
+++ src/test/java/com/dowczarek/logic/TestLogicConfiguration.java
@@ -1,21 +1,13 @@
 package com.dowczarek.logic;
 
-import com.dowczarek.model.Task;
 import com.dowczarek.model.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @Configuration
 class TestLogicConfiguration {
@@ -34,43 +26,6 @@ class TestLogicConfiguration {
     @Primary
     @Profile("integration")
     TaskRepository testTaskRepository() {
-        return new TaskRepository() {
-            private final Map<Integer, Task> tasks = new HashMap<>();
-
-            @Override
-            public List<Task> findAll() {
-                return new ArrayList<>(tasks.values());
-            }
-
-            @Override
-            public Page<Task> findAll(Pageable page) {
-                return null;
-            }
-
-            @Override
-            public Optional<Task> findById(Integer id) {
-                return Optional.ofNullable(tasks.get(id));
-            }
-
-            @Override
-            public boolean existsById(Integer id) {
-                return tasks.containsKey(id);
-            }
-
-            @Override
-            public boolean existsByDoneIsFalseAndGroup_Id(Integer groupId) {
-                return false;
-            }
-
-            @Override
-            public List<Task> findByDone(boolean done) {
-                return null;
-            }
-
-            @Override
-            public Task save(Task entity) {
-                return tasks.put(tasks.size() + 1, entity);
-            }
-        };
+        return new InMemoryTaskRepository();
     }
 }
