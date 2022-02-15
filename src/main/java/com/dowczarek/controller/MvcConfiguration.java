10b08a54cb0033ce343b8562877847b32197c2b3
dowczarek <d_owczarek@outlook.com>
2022-02-15 21:20:03 +0100

    MvcConfiguration - Interceptors dependency injection

--- src/main/java/com/dowczarek/controller/MvcConfiguration.java
+++ src/main/java/com/dowczarek/controller/MvcConfiguration.java
@@ -1,13 +1,22 @@
 package com.dowczarek.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Set;
+
 @Configuration
 class MvcConfiguration implements WebMvcConfigurer {
+    private final Set<HandlerInterceptor> interceptors;
+
+    MvcConfiguration(Set<HandlerInterceptor> interceptors) {
+        this.interceptors = interceptors;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor());
+        interceptors.forEach(registry::addInterceptor);
     }
 }
